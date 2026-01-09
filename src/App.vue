0c6b00504613e2f23bb83f6727e346f5e53b60ea
Jonathan Foucher <46852393+jonathan-foucher@users.noreply.github.com>
2026-01-09 23:26:54 +0100

    allow to pick the number of players

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,8 @@ import { ref } from 'vue'
 import PlayerCard from '@/components/PlayerCard.vue'
 
 const players = ref()
+const nPlayers = ref(1)
+const nPlayersOptions = [...Array(5).keys()].map((i) => i + 1)
 
 const startRandomize = () => {
   for (const player of players.value) {
@@ -15,13 +17,14 @@ const startRandomize = () => {
   <q-layout>
     <q-page-container>
       <q-page class="column main-page">
-        <div class="row justify-around">
-          <q-btn color="amber-10" size="lg" icon-right="refresh" class="q-ma-xl" @click="startRandomize">
+        <div class="row justify-center items-center">
+          <q-btn color="amber-10" size="lg" icon-right="refresh" class="column q-my-xl q-mx-lg" @click="startRandomize">
             Launch
           </q-btn>
+          <q-select filled v-model="nPlayers" :options="nPlayersOptions" bg-color="grey-5" class="column" />
         </div>
         <div class="row justify-around items-center">
-          <PlayerCard v-for="index in 5" :key="index" ref="players" :player-name="`Player ${index}`" />
+          <PlayerCard v-for="index in nPlayers" :key="index" ref="players" :player-name="`Player ${index}`" />
         </div>
       </q-page>
     </q-page-container>
